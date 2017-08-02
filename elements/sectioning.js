15bbfc8f4d7476c564cc42c015fa46bb5bddc921
Maxwell Simmer <maxwell.simmer@gmail.com>
2017-08-02 17:04:44 +0200

    Add sectioning elements

---
+++ elements/sectioning.js
@@ -0,0 +1,24 @@
+(function(define) {
+    define(function (require, exports, module) {
+        return [
+            'address',
+            'article',
+            'aside',
+            'footer',
+            'h1',
+            'h2',
+            'h3',
+            'h4',
+            'h5',
+            'h6',
+            'h7',
+            'header',
+            'hgroup',
+            'nav',
+            'section',
+        ]
+    });
+}(typeof module === 'object' && module.exports && typeof define !== 'function' ?
+    function (factory) { module.exports = factory(require, exports, module); } :
+    define
+));
